9fcd038ff2802c5b07b27652975490cc0318361b
Holly emma Johnson <932400824@dov2-sc-18.local>
2019-07-03 20:43:27 +1200

    menu transitions

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,29 +8,16 @@
       id="about"
       class="menu"
     >
-      <!-- <i class="fas fa-user menu-icon"></i> -->
-      <i class="fas fa-music menu-icon"></i>
+      <i class="fas fa-user menu-icon"></i>
       <div class="content">
-        <!-- <img class="nz-map" src="../assets/nz-map.png" alt="Map of NZ"> -->
-        
-        <!-- <h1>An average day:</h1> -->
-        <!-- <div>
-          <ul>
-            <li><a href="https://pinkbike.com/" target="_blank">Bike.</a></li>
-            <li><a href="https://www.youtube.com/user/RHCPtv" target="_blank">Music.</a></li>
-            <li><a href="https://www.trimble.com/" target="_blank">Work.</a></li>
-          </ul>
-        </div> -->
-
-
-        <!-- <div class="row"><div>Red </div><div class="holly"> H</div><div>ot Chili Peppers</div></div>
-        <div>O</div>
-        <div>L</div>
-        <div>L</div>
-        <div>Y</div> -->
-        <iframe src="https://www.youtube.com/embed/txTqtm58AqM?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          src="https://www.dropbox.com/s/z6ekk77tt2bi8oj/swinger-party.mp4?raw=1"
+          height="100%"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
         <!-- <h1>Holly.</h1> -->
-        
+
         <!-- <ul>
           <li>
             <i class="fas fa-mountain"></i>
@@ -67,7 +54,7 @@
             <i class="fas fa-mountain"></i>
             <p>You can find me out riding my bike.</p>
           </li>
-        </ul> -->
+        </ul>-->
       </div>
     </div>
     <!-- menu: tech -->
@@ -104,39 +91,92 @@
     >
       <i class="fas fa-at menu-icon"></i>
       <div class="content">
-        <div>
-          <ul>
-            <li>
-              <i class="fas fa-map-marker-alt"></i>
-              <a href="https://www.google.com/maps/place/Christchurch/@-43.5127894,172.3188943,10z/data=!3m1!4b1!4m5!3m4!1s0x6d322f4863c5ed01:0x500ef8684799945!8m2!3d-43.5320544!4d172.6362254" target="_blank">Christchurch, New Zealand</a>
-            </li>
-            <li>
-              <i class="fas fa-laptop-code"></i>
-              <a href="https://www.trimble.com/" target="_blank">Trimble</a>
-            </li>
-            <!-- <li>
-              <i class="far fa-envelope"></i>
-              <a href>Email me</a>
-            </li>-->
-            <li>
-              <i class="fab fa-github"></i>
-              <a href="https://www.github.com/hollynz" target="_blank">@hollynz</a>
-            </li>
-            <li>
-              <i class="fab fa-instagram"></i>
-              <a href="https://www.instagram.com/hollaylay" target="_blank">@hollaylay</a>
-            </li>
-            <li>
-              <i class="fab fa-linkedin-in"></i>
-              <a href="https://www.linkedin.com/in/holly-johnson-nz/" target="_blank">Holly Johnson</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <form action="submit">
-            <input type="name">
-            <input type="email">
-          </form>
+        <!-- <img class="nz-map" src="../assets/nz-map.png" alt="Map of NZ"> -->
+        <div class="columns is-8">
+          <div class="column is-two-fifths">
+            <ul>
+              <li>
+                <div class="icon">
+                  <i class="fas fa-map-marker-alt"></i>
+                </div>
+                <a
+                  href="https://www.google.com/maps/place/Christchurch/@-43.5127894,172.3188943,10z/data=!3m1!4b1!4m5!3m4!1s0x6d322f4863c5ed01:0x500ef8684799945!8m2!3d-43.5320544!4d172.6362254"
+                  target="_blank"
+                >Christchurch, New Zealand</a>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fas fa-laptop-code"></i>
+                </div>
+                <div>
+                  <a href="https://www.trimble.com" target="_blank">Trimble</a>
+                </div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fab fa-github"></i>
+                </div>
+                <a href="https://www.github.com/hollynz" target="_blank">@hollynz</a>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fab fa-instagram"></i>
+                </div>
+                <a href="https://www.instagram.com/hollaylay" target="_blank">@hollaylay</a>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </div>
+                <a
+                  href="https://www.linkedin.com/in/holly-johnson-nz/"
+                  target="_blank"
+                >Holly Johnson</a>
+              </li>
+            </ul>
+          </div>
+          <div class="column is-three-fifths">
+            <form action="submit" class="form">
+              <div class="field">
+                <label class="label">Name</label>
+                <p class="control has-icons-left">
+                  <input class="input" type="text" placeholder="Name">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="field">
+                <label class="label">Phone</label>
+                <p class="control has-icons-left">
+                  <input class="input" type="phone" placeholder="xxx-xxx-xxxx">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-phone"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="field">
+                <label class="label">Email</label>
+                <p class="control has-icons-left">
+                  <input class="input" type="email" placeholder="email@email.com">
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="field">
+                <label class="label">Say hello!</label>
+                <div class="control">
+                  <textarea class="textarea" placeholder="Kia ora..."></textarea>
+                </div>
+              </div>
+              <div class="field">
+                <p class="control control-button">
+                  <button class="button">Send</button>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
@@ -181,6 +221,9 @@ export default {
 .notVisible {
   visibility: hidden;
 }
+ul {
+  list-style: none;
+}
 .menu-container {
   margin: 5% auto;
   //   background: rgba(0, 0, 0, 0.2);
@@ -188,36 +231,35 @@ export default {
   flex-direction: row;
   justify-content: space-between;
   overflow: hidden;
-  // position: relative;
   height: 70vh;
   .menu {
     z-index: 2;
     width: 80px;
     height: 80px;
     border: solid 1px rgba(255, 255, 255, 0.1);
-    transition: all 0.4s ease;
+    transition: all 0.5s ease-in-out;
     display: flex;
     justify-content: center;
     align-items: center;
     position: absolute;
     .menu-icon {
+      display: flex;
       font-size: 40px;
       color: #5e7ca0;
       color: rgba(255, 255, 255, 0.3);
     }
-    .content {
-      display: none;
-    }
+    // .content {
+    //   width: 0;
+    //   opacity: 0;
+    //   transform: translateX(500px);
+    //   transition: all 0.8s ease-in-out;
+    // }
     &:hover {
-      // overflow-y: scroll;
       background: rgba(255, 255, 255, 0.9);
-      cursor: auto;
       width: 100%;
       height: 600px;
       margin: 0;
-      // margin-bottom: 50vh;
       transition: all 0.5s ease;
-      // position: absolute;
       .menu-icon {
         display: none;
       }
@@ -229,16 +271,6 @@ export default {
         width: 100%;
         height: 100%;
         padding: 4%;
-        ul {
-          font-size: 1.6em;
-          text-align: left;
-          list-style: none;
-          li {
-            display: flex;
-            flex-direction: row;
-            align-content: center;
-          }
-        }
         .columns {
           height: 100%;
           width: 80%;
@@ -257,12 +289,14 @@ export default {
       }
     }
   }
-  #about {
+  .menu#about {
     left: 0;
     padding: 0;
     .content {
+      width: 0;
+      opacity: 0;
+      transition: width 0.4s ease-in-out, opacity 0.3s ease-in-out;
       padding: 0;
-      width: 100%;
       height: 100%;
       position: relative;
       iframe {
@@ -278,15 +312,79 @@ export default {
         font-size: 4em;
         color: rgba(203, 8, 8, 0.7);
       }
+      body:-webkit-full-page-media {
+        background: transparent !important;
+      }
+    }
+    &:hover {
+      background: transparent;
+      .content {
+        background: transparent;
+        width: 100%;
+        opacity: 1;
+        transition: width 0.3s ease-in-out, opacity 0.9s ease-in-out;
+      }
     }
   }
-  #tech {
+  .menu#tech {
     left: 0;
     right: 0;
     margin: 0 auto;
+    .content {
+      display: none;
+    }
+    &:hover {
+      .content {
+        display: flex;
+      }
+    }
   }
-  #contact {
+  .menu#contact {
     right: 0;
+    .content {
+      display: flex;
+      width: 0;
+      opacity: 0;
+      transform: translateX(2000px);
+      transition: opacity 0.2s ease-in-out, transform 0.3s ease-in-out;
+      ul {
+        margin: 0;
+        font-size: 1.6em;
+        text-align: left;
+        li {
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-start;
+          a {
+            font-size: 0.9em;
+            font-weight: 100;
+          }
+          .icon {
+            margin-top: 3px;
+            margin-right: 14px;
+            font-size: 1em;
+            color: #bd320cd7;
+          }
+        }
+      }
+      form {
+        .label {
+          text-align: left;
+        }
+        .control-button {
+          display: flex;
+          justify-content: center;
+        }
+      }
+    }
+    &:hover {
+      .content {
+        width: 100%;
+        transform: translateX(0);
+        opacity: 1;
+        transition: opacity 0.5s ease-in-out, transform 0.7s ease-in-out;
+      }
+    }
   }
 }
 </style>
